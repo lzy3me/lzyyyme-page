a027fd892558e232043c2234646af03b87900d8d
Jessada Supapsuntorn <26509248+lzy3me@users.noreply.github.com>
2022-12-03 04:19:08 +0700

    Fix on lint recommendation

--- components/profile/contact.tsx
+++ components/profile/contact.tsx
@@ -3,12 +3,12 @@ import styles from "../../styles/Card.module.css";
 
 export default function Contact({ ...props }: any) {
   let { contact } = props;
-  
+
   return (
     <div className={`responsive grid md:grid-cols-2 gap-4 place-content-evenly ${styles._card}`}>
       {contact?.map((data: any) => {
         return (
-          <div className="icon-color flex flex-row gap-2">
+          <div key={data?.id} className="icon-color flex flex-row gap-2">
             <Image
               src={`/images/icons/${data?.type}.svg`}
               alt={data?.type}
@@ -16,7 +16,7 @@ export default function Contact({ ...props }: any) {
               height={25}
             />
             {data?.content?.includes("https://") ? (
-              <a href={data?.content} target="_blank">
+              <a href={data?.content} target="_blank" rel="noreferrer">
                 <p className="border-l-4 border-l-amber-500 pl-2">
                   {data?.content?.slice(8)}
                 </p>
